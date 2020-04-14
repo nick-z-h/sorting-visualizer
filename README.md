1cd25ef016cca3b6e34cd0b4fd6088e1caf648ef
nicklauszhanghoule <n.zhanghoule@gmail.com>
2020-04-14 12:40:39 -0400

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-### Sorting-Visualizer
-The best way to understand algorithms is to see them in action.
-
-## Algorithms Implemented:
-| Bubble Sort |
-| Selection Sort |
-| Insertion Sort |
-| Merge Sort |
-| Quick Sort |
---
+++ precache-manifest.67042b0fc3e574bb1a586436363b864c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c81bfba7070099f12f0c59a5b49dbb",
+    "url": "/sorting-visualizer/index.html"
+  },
+  {
+    "revision": "b75d4c05af88b58b8f7c",
+    "url": "/sorting-visualizer/static/css/main.a9f94659.chunk.css"
+  },
+  {
+    "revision": "ac850d2a847b3dc4887b",
+    "url": "/sorting-visualizer/static/js/2.38d8d508.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sorting-visualizer/static/js/2.38d8d508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75d4c05af88b58b8f7c",
+    "url": "/sorting-visualizer/static/js/main.8bd5e07f.chunk.js"
+  },
+  {
+    "revision": "206927cec987e24fce8f",
+    "url": "/sorting-visualizer/static/js/runtime-main.2a14b4db.js"
+  }
+]);
